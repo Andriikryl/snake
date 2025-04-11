6083928fbf8da1d8657178bca47f9c5e1a5deed3
Andriikryl <andreyk557@gmail.com>
2025-04-11 21:26:18 +0300

    add movement

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+type Direction = "right" | "left" | "up" | "down";
 const canvas = ref<HTMLCanvasElement | null>(null);
+const opposite: Record<Direction, Direction> = {
+  right: "left",
+  left: "right",
+  up: "down",
+  down: "up",
+};
 const x = ref(0);
 const y = ref(0);
+const direction = ref<Direction>("right");
 
 const snake = ref([
   { x: 5, y: 5 },
@@ -13,19 +21,41 @@ onMounted(() => {
   if (!canvas.value) {
     return;
   }
+
   const ctx = canvas.value.getContext("2d");
   if (!ctx) return;
-  setInterval(() => {
-  const head = snake.value[snake.value.length - 1];
-  const newHead = { x: head.x + 1, y: head.y };
-  snake.value.push(newHead);
-  snake.value.shift();
-
-  ctx.clearRect(0, 0, canvas.value!.width, canvas.value!.height);
-  for (const segment of snake.value) {
-    ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
+  const handleKey = (e: KeyboardEvent) => {
+    const keyMap: Record<string, Direction> = {
+    ArrowUp: "up",
+    ArrowDown: "down",
+    ArrowLeft: "left",
+    ArrowRight: "right"
+  };
+  const newDirection = keyMap[e.key];
+  if (newDirection && newDirection !== opposite[direction.value]) {
+    direction.value = newDirection;
   }
-}, 1000);
+  };
+  window.addEventListener("keydown", handleKey);
+
+  onBeforeUnmount(() => {
+    window.removeEventListener("keydown", handleKey);
+  });
+  setInterval(() => {
+    const head = snake.value[snake.value.length - 1];
+    let newHead = { x: head.x, y: head.y };
+    if (direction.value === "right") newHead.x += 1;
+    if (direction.value === "left") newHead.x -= 1;
+    if (direction.value === "up") newHead.y -= 1;
+    if (direction.value === "down") newHead.y += 1;
+    snake.value.push(newHead);
+    snake.value.shift();
+
+    ctx.clearRect(0, 0, canvas.value!.width, canvas.value!.height);
+    for (const segment of snake.value) {
+      ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
+    }
+  }, 1000);
 });
 </script>
 
